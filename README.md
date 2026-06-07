600fa3562cf54002bfa32c0fd0081a08fb9cfd8c
Loonuh <you@example.com>
2026-06-07 20:28:21 +0000

    First Website Commit.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # website
+stuff to add still:
+- on the main page, add dearly beloved with a blurred video of galaxy on loop
+- add a enter site button
+- sounds on hover and click
---
+++ script.js
@@ -0,0 +1,49 @@
+const box = document.getElementById("box");
+const box2 = document.getElementById("box2");
+var moosic = document.getElementById("backgroundmoosic");
+
+const about = document.getElementById("about");
+const btn = document.getElementById("aboutbtn");
+
+btn.addEventListener("click", () => {
+    about.style.display = "flex";
+    requestAnimationFrame(() => {
+        about.classList.add("show");
+    });
+});
+
+box2.style.display = "none";
+
+document.getElementById("fadeBtn").addEventListener("click", () => {
+    document.getElementById("fadeBtn").disabled = true;
+    let opacity = 1;
+
+  const interval = setInterval(() => {
+    opacity -= 0.02;
+    box.style.opacity = opacity;
+
+    if (opacity <= 0) {
+      clearInterval(interval);
+      box.style.display = "none";
+
+        backgroundmoosic.play();
+
+      let opacity2 = 0;
+      box2.style.opacity = 0;
+      box2.style.display = "flex";
+
+      const interval2 = setInterval(() => {
+        opacity2 += 0.05;
+        box2.style.opacity = opacity2;
+
+        if (opacity2 >= 1) {
+          clearInterval(interval2);
+        }
+      }, 50);
+    }
+  }, 50);
+});
+
+document.getElementById("about").addEventListener("click", () => {
+    
+})
