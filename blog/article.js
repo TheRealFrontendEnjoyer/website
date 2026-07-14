8288dfba5f1f47e2a253a135f77e806e011e6bb8
Loonuh's PC <you@example.com>
2026-07-15 00:22:27 +0100

    aaaaaa

--- blog/article.js
+++ blog/article.js
@@ -97,6 +97,41 @@ const articles = {
             <p>Well, that's it! Have a bunnytastic day!!!! &lt;3</p>
         `
     },
+
+    "getting-into-new-not-so-new-stoof": {
+        pageTitle: 'getting into new (not so new) stoof', // title tag
+        title: 'getting into new (not so new) stoof', // title of the article itself
+        date: "15 july 2026", // date of the article
+        content: `
+            <p>So, for the past few weeks, I've been doing the same stuff over and over again..</p>
+
+            <p>And honestly, I'm tired of it! That's why I've been getting into new.. And also not new stuff!</p>
+
+            <p>In terms of games, I've been getting more into Rainbow Six Siege, I had the game for probably 4 years at this point but never got into it!</p>
+
+            <p>After a few hours of gaming, it's SO GOOD AAAAAAAAAAAAAAA</p>
+
+            <p>The problem is, you take forever to unlock competitive, I have 45 hours in total and I'm still level 35.. (you need lvl 50 to get comp)</p>
+
+            <p>I've been also making moosic again! Mainly ambience.. Mixed with lots of breaks..</p>
+
+            <p>Honestly, I blame Sewersvlt for the way I make music! (the first time I did anything ambience like was a tutorial based on their music)</p>
+
+            <p>And I'm super happy because.. Ambience music is fun!</p>
+
+            <p>ALSO! I've been getting into COD BO2! And I know what you're thinking.. "Girl, that game's SUPER OLD!"</p>
+
+            <p>And to that, I say.. True.. BUT! It's part of my childhood.</p>
+
+            <p>Actually, the first COD I played was BO3, I barely played BO2 as a kid but I watched way too much content on BO2.</p>
+
+            <p>And before I forget, except a blog update on either... Friday or Saturday!</p>
+
+            <p>I'm gonna get my final exam grades and I'll share here with y'all, silly people behind the screen! :3</p>
+
+            <p>Have a bunnytastic day!!!! &lt;3</p>
+        `
+    },
 };
 
 const params = new URLSearchParams(window.location.search);
